4e14013b14ef8eba669a20cbf6a91ec963e53131
Farideh <heidarian89@gmail.com>
2023-12-25 23:19:47 -0800

    add qty manager function to project

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,27 +9,40 @@ const totalQty = document.querySelector("#total-qty");
 //   { id: 2, image: "", title: "", qty: 5 },
 // ];
 
+const btnAddToCart = ` <button class="btn bg-transparent" data-btn="add-to-cart"> <i
+                                                    class="bi bi-cart-fill text-white rounded-circle px-2 py-1 bg-warning"></i></button>`;
+
 addToCart_btns.forEach((item) => {
   item.addEventListener("click", (e) => {
     ++totalQty.innerText;
 
-    const productInfo = getInfoProduct(e.target);
+    const product = e.target.closest(".card");
+    qtyManage(product);
+    const productInfo = getInfoProduct(product);
     const id = productInfo.idProduct;
     const itemCart = document.querySelector(`div[data-cart='${id}']`);
-
 
     if (itemCart) {
       ++itemCart.querySelector(".qty-item").innerText;
     } else {
       createItemCart(productInfo);
     }
-
   });
 });
+const qtyManage = (product) => {
+  const qtyBox = `<div class="shadow text-warning bg-white p-2 rounded">
+  <span class="p-2">-</span>
+  <span class="p-2">1</span>
+  <span class="p-2">+</span>
+</div>`;
 
-const getInfoProduct = (target) => {
-  const product =
-    target.parentElement.parentElement.parentElement.parentElement;
+  product.querySelector(".add-to-cart-box").innerHTML = qtyBox;
+};
+
+const getInfoProduct = (product) => {
+  // const product =
+  // target.parentElement.parentElement.parentElement.parentElement.parentElement;
+
   const idProduct = product.id;
   const titleProduct = product.querySelector(".card-title").innerText;
   const imgProduct = product.querySelector("img").src;
@@ -48,7 +61,7 @@ const createItemCart = (productInfo) => {
                                             <div class="card-body">
                                                 <div  class="d-flex justify-content-between align-items-center">
                                                     <h5 class="card-title">${productInfo.titleProduct}</h5>
-                                                    <i class="bi bi-trash3-fill text-danger"></i>
+                                                    <i class="bi bi-trash3-fill text-danger" id="deleteItem"></i>
                                                 </div>
                                                 <div class="d-flex justify-content-between align-items-center">
                                                     <p class="card-text mb-0">
@@ -62,3 +75,22 @@ const createItemCart = (productInfo) => {
                                 </div>`;
   document.querySelector(".cart-items").innerHTML += itemCart;
 };
+
+const cartItems = document.querySelector(".cart-items");
+
+cartItems.addEventListener("click", (e) => {
+  if (e.target.id === "deleteItem") {
+    const itemCart = e.target.closest(".item-cart");
+    const qtyItem = itemCart.querySelector(".qty-item");
+    // const qty = parseInt(qtyItem.innerText);
+
+    if (qtyItem.innerText > 0) {
+      --totalQty.innerText;
+      --qtyItem.innerText;
+
+      if (qtyItem.innerText < 1) {
+        itemCart.remove();
+      }
+    }
+  }
+});
